72a1e85867f0c85e45533faeeec86caf2965fb26
Muhammad Suleman <msulemanaslam11@gmail.com>
2025-05-31 19:37:21 +0500

    update page.js

--- pixelpirates/src/app/page.js
+++ pixelpirates/src/app/page.js
@@ -1,9 +1,40 @@
-import Image from "next/image";
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { supabase } from "@/lib/supabaseClient";
+import Login from "@/components/Login";
 
 export default function Home() {
-  return (
-    <div className="bg-sky-600 h-full w-full">
-      hshsjjs
-    </div>
-  );
+  const router = useRouter();
+
+  useEffect(() => {
+    const checkSessionAndRedirect = async () => {
+      const {
+        data: { user },
+      } = await supabase.auth.getUser();
+
+      if (user) {
+        // Get user role from `profiles` table
+        const { data: profile } = await supabase
+          .from("profiles")
+          .select("role")
+          .eq("id", user.id)
+          .single();
+
+        // Redirect based on role
+        if (profile?.role === "admin") {
+          router.push("/admindashboard");
+        } else if (profile?.role === "official") {
+          router.push("/OfficialDashboard");
+        } else if (profile?.role === "citizen") {
+          router.push("/citizendashboard");
+        }
+      }
+    };
+
+    checkSessionAndRedirect();
+  }, [router]);
+
+  return <Login />;
 }
