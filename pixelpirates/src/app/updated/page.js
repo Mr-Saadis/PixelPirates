0743774bb12c4222c3029bb55d220188244e06b9
Muhammad Saad Shahid <fa23-bce-068@cuilahore.edu.pk>
2025-05-31 20:38:26 +0500

    final

--- pixelpirates/src/app/updated/page.js
+++ pixelpirates/src/app/updated/page.js
@@ -5,7 +5,7 @@ import AdminPanel from '@/components/AdminPanel';
 export default function AdminPage() {
   return (
     <div className="min-h-screen bg-[#1D3557] text-white p-6">
-      <h1 className="text-2xl font-bold mb-6 text-center">🛠️ Admin Issue Manager</h1>
+      <h1 className="text-2xl font-bold mb-6 text-center">🛠️ Officials Issue Manager</h1>
       <AdminPanel />
     </div>
   );
